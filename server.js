5d05d6bbc49c720d492bc8e2ab39bc4e1e11f1df
GuillianV <guillianvibert90@gmail.com>
2024-01-16 00:30:01 +0100

    feat: manifest

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 import { EPub } from 'epub2';
 import * as fs from "fs";
-import { dirname , join, extname} from "path";
+import { dirname, join, extname } from "path";
 import { fileURLToPath } from 'url';
 import chalk from 'chalk';
+import util from 'util'
+import express from "express";
 
+const app = express();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
@@ -14,27 +17,74 @@ const filename = "book.epub"
 const ReadEpubFile = async (filename, filepath) => {
 
     const fullpath = join(filepath, filename);
-    if(!fs.existsSync(fullpath)) {
+    if (!fs.existsSync(fullpath)) {
         console.error(chalk.red(`File not found : ${fullpath}`));
         return;
     }
 
 
     const extension = extname(filename);
-    if(extension !== ".epub") {
+    if (extension !== ".epub") {
         console.error(chalk.red(`File is not type of epub`));
         return;
     }
 
     const filenameWithoutExtension = filename.replace(extension, "");
-    fs.mkdirSync(join(dataPath,filenameWithoutExtension,"images"), { recursive: true });
-    fs.mkdirSync(join(dataPath,filenameWithoutExtension,"links"), { recursive: true });
+    fs.mkdirSync(join(dataPath, filenameWithoutExtension, "images"), { recursive: true });
+    fs.mkdirSync(join(dataPath, filenameWithoutExtension, "links"), { recursive: true });
 
 
-    let epub = await EPub.createAsync(fullpath, join(dataPath,filenameWithoutExtension,"images"), join(dataPath,filenameWithoutExtension,"links"));
-    console.log(epub)
+    let epub = await EPub.createAsync(fullpath, join(dataPath, filenameWithoutExtension, "images"), join(dataPath, filenameWithoutExtension, "links"));
 
+    Object.values(epub.manifest).forEach((item) => {
+
+        const path = join(dataPath, filenameWithoutExtension, "images", item.id)
+        if (!fs.existsSync(path)) {
+
+            epub.getImage(item.id, (error, data, mimeType) => {
+                if (data)
+                    fs.writeFile(join(dataPath, filenameWithoutExtension, "images", item.id), data, (err) => {
+                        if (err) {
+                            console.error(chalk.red(`Error while writing file ${item.id}`));
+                            return;
+                        }
+                        console.log(chalk.green(`File ${item.id} has been saved!`));
+                    });
+            })
+
+           
+        }
+        
+    })
+
+
+  
+    app.get("/:chapter", (req, res) => {
+
+        epub.getChapterRaw(epub.flow[req.params.chapter ?? 0].id, (error, text) => {
+
+
+            res.send(text);
+          
+
+        })
+
+    });
 }
 
 
 ReadEpubFile(filename, inputPath);
+
+
+app.use('/:asset', (req, res, next) => {
+  express.static(__dirname + '/data/book/images/'+ req.params.asset)(req, res, next);
+});         
+
+
+
+// PORT
+const PORT = 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on PORT: ${PORT}`);
+});
